2abcfbe07c473438fbbf8363d2f28b2649c2d045
Islam Yamor <islamyamorz@gmail.com>
2025-11-02 08:38:46 +0300

    added support for environments variables

---
+++ app.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    EXPO_PUBLIC_TOLGEE_API_URL: string
+    EXPO_PUBLIC_TOLGEE_API_KEY: string
+    TOLGEE_PROJECT_ID: number | string
+  }
+}
